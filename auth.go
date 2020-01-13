ebc762e2f7cf7b15f505dc529db38f083863a396
jeff.kim <jeff.kim@groundx.xyz>
2020-01-13 15:42:26 +0900

    Test auth

--- auth.go
+++ auth.go
@@ -9,20 +9,49 @@ package main
 import (
 	"crypto/hmac"
 	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
 	"strconv"
+	"strings"
 	"time"
 )
 
 // auth _
-func auth(requset http, key, verb, path, data string, secret []byte) { //(string key, secret) {
+func auth(req *http.Request, key, verb, path, data, secret string) { //(string key, secret) {
 	// unix timestamp + key  + secret
 	expire := time.Now().Add(time.Minute * 5).Unix()
-
-	h := hmac.New(sha256.New, secret)
-	h.Sum([]byte(verb + path + strconv.FormatInt(expire, 10) + data))
+	req.Header.Add("api-expires", strconv.FormatInt(expire, 10))
+	req.Header.Add("api-key", key)
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write([]byte(verb + path + strconv.FormatInt(expire, 10) + data))
+	req.Header.Add("api-signature", hex.EncodeToString(h.Sum(nil)))
+	// DebugHttpRequest(req)
+	return
 
 }
 
-func getWallet() {
+func DebugHttpRequest(r *http.Request) {
+	requestDump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(requestDump))
+}
 
+func Signature(apiSecret, method, path, query, nonce, bodyStr string) string {
+	str := ""
+	if "" == query {
+		str = strings.ToUpper(method) + path + nonce + bodyStr
+	} else {
+		str = strings.ToUpper(method) + path + "?" + query + nonce + bodyStr
+	}
+	return CalSignature(apiSecret, str)
+}
+
+func CalSignature(apiSecret, payload string) string {
+	sig := hmac.New(sha256.New, []byte(apiSecret))
+	sig.Write([]byte(payload))
+	return hex.EncodeToString(sig.Sum(nil))
 }
--- main.go
+++ main.go
@@ -19,12 +19,25 @@ func main() {
 		Host:   "testnet.bitmex.com",
 		Path:   "api/v1",
 	}
-	fmt.Println(u.String() + "/wallet")
-	resp, err := http.Get(u.String() + "/user/wallet")
+	client := http.Client{}
+	req, err := http.NewRequest("GET", u.String()+"/user/wallet", nil)
 	if nil != err {
 		panic(err.Error())
 	}
-	fmt.Println(u.String())
+	key := ""
+	verb := "GET"
+	path := "/api/v1/user/wallet"
+	data := ""
+	secret := ""
+	auth(req, key, verb, path, data, secret)
+	fmt.Println(req.Header.Get("api-expires"))
+	fmt.Println(req.Header.Get(("api-key")))
+	fmt.Println(req.Header.Get("api-signature"))
+	resp, err := client.Do(req)
+	// resp, err := http.Get(u.String() + "/user/wallet")
+	if nil != err {
+		panic(err.Error())
+	}
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(resp.Body)
 	newStr := buf.String()
